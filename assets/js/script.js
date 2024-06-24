f195df0fc357bf5b6fbef6a153d21b8b1b356898
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-06-24 10:55:30 +0700

    add shadow header

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,11 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('header-bg') : header.classList.remove('header-bg');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
